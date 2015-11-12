13dbcb626b9198c1979b1ef5034ef281d8c94b38
Carolina Defranceschi <karodefranceschi@gmail.com>
2015-11-12 15:14:52 -0300

    agrego dropdown brand a la barra de navegacion

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,20 @@
 class WelcomeController < ApplicationController
+	before_action :load_data, only: [:index, :brand]
 
    def index
     @products = Product.all
+   
+  end
+
+  def brand
+  	@brands = Brand.all
+  	render 'index'
+  end
+
+#ordeno las marcas por orden alfabetico
+  def load_data
+    @brands = Brand.order(:name).all
+    
   end
   
 end
