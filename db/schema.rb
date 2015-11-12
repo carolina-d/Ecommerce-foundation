ba1f83c1047125eccc4182b787ec104017ece3c2
Carolina Defranceschi <karodefranceschi@gmail.com>
2015-11-12 17:38:43 -0300

    scaffold category

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112182755) do
+ActiveRecord::Schema.define(version: 20151112203615) do
 
   create_table "brands", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
