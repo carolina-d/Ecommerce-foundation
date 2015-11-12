066493bce2b5f6988e5567630abcfaab39821057
Carolina Defranceschi <karodefranceschi@gmail.com>
2015-11-12 13:15:35 -0300

    generando scaffold brand

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112142627) do
+ActiveRecord::Schema.define(version: 20151112161433) do
+
+  create_table "brands", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
