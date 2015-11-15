3249e8e4f80c375bd089baa1cfd2a8aa9b074944
Carolina Defranceschi <karodefranceschi@gmail.com>
2015-11-15 12:36:39 -0300

    agrego configuracion para la gem mercadopago

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,9 @@ Rails.application.configure do
 
   #configuracion para la gema devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  #configuracion para mercado pago
+  config.after_initialize do
+    Rails.application.routes.default_url_options[:host] = "localhost:3000"
+  end
 end
